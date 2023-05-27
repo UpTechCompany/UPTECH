3de0fec69ae06316ced970937a5d1e7f7da18de3
Tenshiqq <zakan.6000@gmail.com>
2023-05-27 10:47:11 +0800

    final

--- app/src/main/java/com/example/uptechapp/dao/RetrofitService.java
+++ app/src/main/java/com/example/uptechapp/dao/RetrofitService.java
@@ -5,7 +5,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class RetrofitService {
 
-    private static final String BASE_URL = "http://158.160.51.73:8080";
+    private static final String BASE_URL = "http://51.250.14.236:8080";
 
     private static Retrofit retrofit;
 
